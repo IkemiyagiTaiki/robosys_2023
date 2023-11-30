df04242fe58316523c2af3c2f755bfd70e4cdbed
Ikemiyagi Taiki <taikiike@icloud.com>
2023-11-30 16:02:52 +0900

    Fix a sqrt

---
+++ test_sqrt.bash
@@ -0,0 +1,24 @@
+#!/bin/bash  
+# SPDX-FileCopyrightText: 2023 IkemiyagiTaiki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$( ./sqrt 5 )
+[ "${out}" = 2.23606797749979  ] || ng $LINENO
+
+### STRANGE INPUT ###
+out=$(echo | ./sqrt)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
+
